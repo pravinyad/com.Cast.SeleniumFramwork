af5b36667d44b1b5a1913249cef215c172c51eee
pravinyad <pravinyadav010@gmail.com>
2021-09-21 18:03:56 -0700

    first commit comcast

---
+++ src/test/java/com/comcast/contacttest/ContactTest.java
@@ -0,0 +1,22 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContatc1Test() {
+		System.out.println("execute 1");
+		
+	}
+	@Test
+	public void createContatc2Test() {
+		System.out.println("execute 2");
+		
+	}
+	@Test
+	public void createContatc3Test() {
+		System.out.println("execute 3");
+		
+	}
+
+}
